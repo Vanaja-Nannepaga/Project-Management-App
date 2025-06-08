8ded4f1a9b8af4167363071b97d07dc99626b06f
Vanaja-Nannepaga <itsmevanaja08@gmail.com>
2025-06-08 14:52:51 +0530

    Day 4: Implemented ticket model and backend APIs with auth middleware

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const ticketRoutes = require('./routes/ticketRoutes');
 
 // Load environment variables
 dotenv.config();
-
 
 const app = express();
 
@@ -18,6 +19,8 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tickets', ticketRoutes);
 
 // Test route
 app.get('/', (req, res) => {
